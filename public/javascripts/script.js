0ff1d04d288be89225f27614c3caf338c0720e1c
Edson Luiz Fragnan <edsonfragnan@brq.com>
2017-11-30 18:44:31 -0200

    Alterações

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,49 +1,88 @@
-const sacola = [];
+const sacolaControle = [];
 const AddCarrinho = (produto) => {
-  sacola.push(produto);
   const novaSacola = [];
-  for (const i in sacola) {
-    novaSacola.push(
-      '<div class="col-xs-12 col-md-12 col-lg-12" id="'+ sacola[i].sku +'">' +
-        '<div class="thumbnail thumb2">' +
-          '<img src="images/corinthians.jpg" class="" alt="Corinthians" style="width:100%">' +
-        '</div>'+
-        '<div class="descPedido">' +
-          '<p class="descProduto">' + sacola[i].title + '<span class="glyphicon glyphicon-remove" aria-hidden="true" onclick="RemoveProduto('+ sacola[i].sku +');"><strong>X</strong></span></p>' +
-          '<p class="tamanhoProduto">' + sacola[i].availableSizes + ' | ' + sacola[i].style + '</p>' +
-          '<p class="quantidade">Quantidade: 2 <span class="precProdutoCarrinho">' + sacola[i].price + '</span></p>' +
-        '</div>' +
-      '</div>'
-    );
-    sessionStorage.setItem('storage', novaSacola);
-  }
+  novaSacola.push(
+    '<div class="col-xs-12 col-md-12 col-lg-12" id="'+ produto.sku +'">' +
+      '<div class="thumbnail thumb2">' +
+        '<img src="images/corinthians.jpg" class="" alt="Corinthians" style="width:100%">' +
+      '</div>'+
+      '<div class="descPedido">' +
+        '<p class="descProduto">' + produto.title + '<span class="glyphicon glyphicon-remove" aria-hidden="true" onclick="RemoveProduto('+ produto.sku +');"><strong>X</strong></span></p>' +
+        '<p class="tamanhoProduto">' + produto.availableSizes + ' | ' + produto.style + '</p>' +
+        '<p class="quantidade">Quantidade: 2 <span class="precProdutoCarrinho">' + produto.price + '</span></p>' +
+      '</div>' +
+    '</div>');
+  const objetProduto = {
+    'id': produto.id,
+    'sku': produto.sku,
+    'title': produto.title,
+    'description': produto.description,
+    'availableSizes': produto.availableSizes,
+    'style': produto.style,
+    'price': produto.price,
+    'installments': produto.installments,
+    'currencyId': produto.currencyId,
+    'currencyFormat': produto.currencyFormat,
+    'isFreeShipping': produto.isFreeShipping
+  };
   const divContador = document.getElementById('contador');
   const div = document.getElementById('produtosSacola');
-  divContador.innerHTML = novaSacola.length;
-  div.innerHTML = novaSacola;
+  const divContadorSacola = document.getElementById('quantidade');
+  sacolaControle.push(objetProduto);
+  sessionStorage.setItem('storage', JSON.stringify(sacolaControle));
+  divContador.innerHTML = sacolaControle.length;
+  sessionStorage.setItem('contador',  sacolaControle.length);
+  const parseStorage = JSON.parse(sessionStorage.getItem('storage'));
+  if (parseStorage === 0) {
+    divContadorSacola.removeChild(divContadorSacola.childNodes[0]);
+  }
+  div.innerHTML+= novaSacola;
 };
 
 const RemoveProduto = (idProduto) => {
-  const elem = document.getElementById(idProduto);
-  const elemCarrinho = document.getElementById('produtosSacola');
-  const sacolaRemove = [];
-  //elem.parentNode.removeChild(elem);
-  //console.log(sessionStorage.getItem('storage'));
-
-  //sessionStorage.removeItem(elem);
-  //sessionStorage.setItem('novoStorage', elemCarrinho.children);
-  /*const divContador = document.getElementById('contador');
-  console.log(sacola.length);
-  divContador.innerHTML = sacola.length - 1;*/
+  const parseStorage = JSON.parse(sessionStorage.getItem('storage'));
+  const produtosSessao = sessionStorage.getItem('storage');
+  const div = document.getElementById('produtosSacola');
+  const divContador = document.getElementById('contador');
+  const divContadorSacola = document.getElementById('quantidade');
+  for (const i in parseStorage) {
+    if (parseStorage[i].sku === idProduto) {
+      parseStorage.splice(i, idProduto);
+    }
+  };
+  const sacola = formataSacola(produtosSessao);
+  if (sessionStorage.getItem('contador') <= 0) {
+    divContador.innerHTML = sacolaControle.length;
+    div.innerHTML = sacola;
+    divContadorSacola.innerHTML = 'Carrinho vazio.';
+  } else {
+    divContador.innerHTML = sessionStorage.getItem('contador');
+    div.innerHTML = sacola;
+  }
+  sessionStorage.setItem('storage', parseStorage)
+  div.innerHTML = sacola;
 };
 
 const SessionCarrinho = () => {
-  const produtosSessao = sessionStorage.getItem('storage');
-  console.log(produtosSessao);
   const div = document.getElementById('produtosSacola');
   const divContador = document.getElementById('contador');
-  //divContador.innerHTML = produtosSessao.length;
-  div.innerHTML = produtosSessao;
+  const divContadorSacola = document.getElementById('quantidade');
+  divContador.innerHTML = sessionStorage.getItem('contador');
+  if (sessionStorage.getItem('contador') <= 0) {
+    divContador.innerHTML = sacolaControle.length;
+    divContadorSacola.innerHTML = 'Carrinho vazio.';
+  } else {
+    const produtosSessao = sessionStorage.getItem('storage');
+    const sacola = formataSacola(produtosSessao);
+    divContador.innerHTML = sessionStorage.getItem('contador');
+    if (sessionStorage.getItem('contador') <= 0) {
+      divContador.innerHTML = sacolaControle.length;
+      divContadorSacola.innerHTML = 'Carrinho vazio.';
+    } else {
+      divContador.innerHTML = sessionStorage.getItem('contador');
+      div.innerHTML = sacola;
+    }
+  }
 };
 
 const FechaCompra = () => {
@@ -51,3 +90,27 @@ const FechaCompra = () => {
 };
 
 const Preco = () => {};
+
+const formataSacola = (produtos) => {
+  if (!produtos) {
+    return sessionStorage.setItem('contador', 0);
+  } else {
+    const sacola = [];
+    const produto = JSON.parse(produtos);
+    for (const i in produto) {
+      sacola.push(
+        '<div class="col-xs-12 col-md-12 col-lg-12" id="'+ produto[i].sku +'">' +
+          '<div class="thumbnail thumb2">' +
+            '<img src="images/corinthians.jpg" class="" alt="Corinthians" style="width:100%">' +
+          '</div>'+
+          '<div class="descPedido">' +
+            '<p class="descProduto">' + produto[i].title + '<span class="glyphicon glyphicon-remove" aria-hidden="true" onclick="RemoveProduto('+ produto[i].sku +');"><strong>X</strong></span></p>' +
+            '<p class="tamanhoProduto">' + produto[i].availableSizes + ' | ' + produto[i].style + '</p>' +
+            '<p class="quantidade">Quantidade: 2 <span class="precProdutoCarrinho">' + produto[i].price + '</span></p>' +
+          '</div>' +
+        '</div>'
+      );
+    }
+    return sacola;
+  }
+};
