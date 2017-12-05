ded98627aa10530ebeab2610b6d7044564d5efec
Edson Luiz Fragnan <edsonfragnan@brq.com>
2017-12-05 18:13:41 -0200

    APP

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -33,19 +33,19 @@ const SessionCarrinho = () => {
   const divMensagemSacola = document.getElementById('mensagemSacola');
   const divSacola = document.getElementById('produtosSacola');
   if (JSON.parse(carrinho) === null || JSON.parse(carrinho).length === 0 || !localStorage.getItem('carrinhoControle') || localStorage.getItem('valorFinal') === '0') {
+    carrinhoControle = [];
     localStorage.setItem('contador', '0');
     localStorage.setItem('carrinhoControle', '');
     localStorage.removeItem('valorFinal');
+    localStorage.removeItem('valorSacola');
     divMensagemSacola.innerHTML = 'Sacola vazia!';
     divValor.innerHTML = 'R$0,00';
     divContador.innerHTML = '0';
   } else {
     sacola = formataSacola(localStorage.getItem('carrinhoControle'));
     valorFinal = localStorage.getItem('valorFinal');
+    valorFinal = parseFloat(valorFinal);
     localStorage.setItem('valorSacola', valorFinal);
-    const cifrao = valorFinal.replace('R$', '');
-    const ponto = cifrao.replace(',', '.');
-    valorFinal = parseFloat(ponto);
     divValor.innerHTML = valorFinal.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL'});
     divContador.innerHTML = carrinho;
     for (const i in sacola) {
@@ -61,15 +61,21 @@ const RemoveProduto = (idProduto) => {
   const trataRemove = formataSacolaRemove(localStorage.getItem('carrinhoControle'), idProduto, localStorage.getItem('valorFinal'));
   const divValorTotal = document.getElementById('valorTotal');
   const divMensagemSacola = document.getElementById('mensagemSacola');
-  localStorage.setItem('valorFinal', trataRemove.valorSub);
+  divContador.innerHTML = JSON.parse(localStorage.getItem('contador') - 1);
   localStorage.setItem('contador', JSON.parse(localStorage.getItem('contador') - 1));
-  localStorage.setItem('carrinhoControle', JSON.stringify(trataRemove.produto));
-  divContador.innerHTML = localStorage.getItem('contador');
-  if (trataRemove.valorSub === 0 || trataRemove.produto.length === 0) {
-    localStorage.setItem('valorSacola', 0);
+  if (trataRemove.valorSub === 0 || trataRemove.produto.length === 0 || localStorage.getItem('contador') === '0') {
+    carrinhoControle = [];
+    localStorage.removeItem('contador');
+    localStorage.setItem('valorSacola', '0');
+    localStorage.setItem('valorFinal', '0');
+    localStorage.setItem('contador', '0');
+    divContador.innerHTML = '0';
     divValorTotal.innerHTML = 'R$0,00';
     divMensagemSacola.innerHTML = 'Sacola vazia!';
   } else {
+    localStorage.setItem('valorFinal', trataRemove.valorSub);
+    localStorage.setItem('valorSacola', trataRemove.valorSub);
+    localStorage.setItem('carrinhoControle', JSON.stringify(trataRemove.produto));
     divValorTotal.innerHTML = trataRemove.valorSub.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL'});
   }
 };
@@ -132,7 +138,7 @@ const trataValorCarrinho = (sacola) => {
   sacola = JSON.parse(sacola);
   precos.push(sacola[0]);
   if (precos.length === 1) {
-    return sacola[0].price;
+    return trataCifrao(sacola[0].price);
   } else {
     for (const i in precos) {
       const cifrao = precos[i].price.replace('R$', '');
@@ -159,43 +165,43 @@ const formataProduto = (produto) => {
   return produtoHtml;
 };
 
-const montaCarrinho = (produto) => {
+const montaCarrinho = (contador) => {
   const divSacola = document.getElementById('produtosSacola');
   const divContador = document.getElementById('contador');
   const divValorTotal = document.getElementById('valorTotal');
   const mensagemSacola = document.getElementById('mensagemSacola');
-  const produtosSacola = produto === undefined || produto === null ? formataSacola(localStorage.getItem('carrinho')) : formataSacola(produto);
+  const produtosSacola = formataSacola(localStorage.getItem('carrinho'));
   let valorTotal = trataValorCarrinho(localStorage.getItem('carrinho'));
+  valorTotal = somaCarrinho(valorTotal, localStorage.getItem('valorSacola'));
   localStorage.setItem('valorFinal', valorTotal);
-  valorTotal = trataCifrao(valorTotal, localStorage.getItem('valorSacola'));
-  localStorage.setItem('valorFinal', valorTotal);
+  if (JSON.parse(localStorage.getItem('contador')) === 1) {
+    let val = JSON.parse(localStorage.getItem('carrinho'));
+    valorTotal = trataCifrao(val[0].price);
+    localStorage.setItem('valorFinal', valorTotal);
+  }
   divSacola.innerHTML+= produtosSacola;
   divValorTotal.innerHTML = valorTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL'});
   divContador.innerHTML = localStorage.getItem('contador');
   mensagemSacola.innerHTML = '';
 };
 
-const trataCifrao = (valor, valor2) => {
+const trataCifrao = (valor) => {
   let cifrao = '';
   let ponto = '';
+  if (valor.indexOf('R$') != -1) {
+    cifrao = valor.replace('R$', '');
+    ponto = cifrao.replace(',', '.');
+    return parseFloat(ponto);
+  } else {
+    return parseFloat(valor);
+  }
+};
+
+const somaCarrinho = (valor, valor2) => {
   if (valor2 === null) {
     return valor
   } else {
-    if (valor.indexOf('R$') != -1 && valor2.indexOf('R$') != -1) {
-      cifrao = valor.replace('R$', '');
-      ponto = cifrao.replace(',', '.');
-      const cifrao2 = valor2.replace('R$', '');
-      const ponto2 = cifrao.replace(',', '.');
-      return parseFloat(ponto) + parseFloat(ponto2);
-    } else if (valor.indexOf('R$') != -1) {
-      cifrao = valor.replace('R$', '');
-      ponto = cifrao.replace(',', '.');
-      return parseFloat(ponto) + parseFloat(valor2);
-    } else {
-      cifrao = valor2.replace('R$', '');
-      ponto = cifrao.replace(',', '.');
-      return valor + parseFloat(ponto);
-    }
+    return valor + parseFloat(valor2);
   }
 };
 
