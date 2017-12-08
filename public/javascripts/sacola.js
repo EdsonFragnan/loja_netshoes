e14fa3ba7d5710384b78789c6443d1f0dfeec07d
Edson Luiz Fragnan <edsonfragnan@brq.com>
2017-12-08 13:10:16 -0200

    Pré-Carrinho

--- public/javascripts/sacola.js
+++ public/javascripts/sacola.js
@@ -1,16 +1,29 @@
 const AddCarrinho = (produto) => {
   const item = document.getElementById('produto');
   const titulo = document.getElementById('myModalLabel');
+  let especificacao = produto.style === '' ? 'Não especificado.' : produto.style;
   const produtoHtml = [
     '<div class="col-xs-12 col-md-12 col-lg-12"  class="modal-body" id="'+ produto.sku +'">' +
       '<div class="thumbnail">' +
         '<img src="images/corinthians.jpg" class="" alt="Corinthians" style="width:100%">' +
       '</div>'+
-      '<div class="">' +
-        '<label>Tamanho: </label>' +
-        '<p><select class="selectpicker">' + tamanho(produto.availableSizes) + '</select></p>' +
-        '<label>Quantidade: </label>' +
-        '<p><input type="text" /></p>' +
+      '<div>' +
+        '<div class="form-group">' +
+          '<label>Estilo: </label>' +
+          '<p>' + especificacao + '</p>' +
+        '</div>' +
+        '<div class="form-group">' +
+          '<label>Tamanho: </label>' +
+          '<p><select class="selectpicker">' + tamanho(produto.availableSizes) + '</select></p>' +
+        '</div>' +
+        '<div class="form-group">' +
+          '<label for="quantidade">Quantidade:</label>' +
+          '<input type="text" class="form-control" id="quantidade" value="1" />' +
+        '</div>' +
+        '<div class="form-group">' +
+          '<label>Preço unidade: </label>' +
+          '<p>' + produto.price + '</p>' +
+        '</div>' +
       '</div>' +
     '</div>'
   ];
